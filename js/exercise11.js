b1157af2f8a68bcffb2463585efca050cb9df4a7
KatieMari <katie.glover@student.norwichuni.ac.uk>
2025-10-07 11:54:46 +0100

    AA

--- js/exercise11.js
+++ js/exercise11.js
@@ -1,12 +1,21 @@
 // LOGGING OBJECTS
 
 // 1. Create a constant person object with name 'Mary' and age 25
-
+const personOne = {
+    name: "Mary",
+    age: 25
+};
 // 2. Create a constant person object with name 'Mark' and age 22
-
+const personTwo = {
+    name: "Mark",
+    age: 22
+};
 // 3. Create a function that logs a the person's name and age
-
+function logPerson (person){
+   console.log(person.name)
+   console.log(person.age)
+};
 // 4. Use the function to log Mary's details
-
+logPerson (personOne);
 // 5. Use the function to log Mark's details
-
+logPerson (personTwo);
--- ts/exercise15.ts
+++ ts/exercise15.ts
@@ -8,9 +8,21 @@ const c = "c";
 const abc = {a, b, c}
 
 // 1. Log the properties a, b and c of the object abc
-
+console.log (abc.a, abc.b, abc.c);
 // 2. Using this technique, create a new function that defines an object animals with two or three animal names and returns the object
-
+function Animals(){
+const dolphine = "dolphine";
+const penguin = "penguin";
+const seal = "seal";
+const animalObject= {dolphine, penguin, seal}
+return animalObject;
+};
 // 3. Create a constant variable myAnimals and use the function to define it
+const myAnimals = Animals();
+  
+
 
 // 4. Log the variable myAnimals
+console.log (myAnimals.dolphine);
+console.log (myAnimals.penguin);
+console.log (myAnimals.seal);
