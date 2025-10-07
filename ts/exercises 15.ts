7fdca6dcd321c93a5022701c5e1f4eb5fd15ebbc
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2025-10-07 08:10:01 +0100

    objects without named variables

---
+++ ts/exercises 15.ts
@@ -0,0 +1,10 @@
+// UNNAMED OBJECTS
+
+// In this example, instead of specifying the key for the object variables, the objects will take the as key, the name of the variable they originated from:
+const a = "a";
+const b = "b";
+const c = "c";
+
+const abc = {a, b, c}
+
+// 1. Log the properties a, b and c of the object abc
